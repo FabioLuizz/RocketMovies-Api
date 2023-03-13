9a2211ca64d14fc0bcb9149992555b0f9b706aaa
FabioLuizz <fabioluis19341@gmail.com>
2023-03-13 11:03:06 -0300

    Refactor

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: path.resolve(__dirname, "Src", "Database", "database.db")
+      filename: path.resolve(__dirname, "src", "database", "database.db")
     },
 
     migrations: {
